9c4b8a186d09f366ff02222572ebfac977abd40a
Vinicius Mascarenhas <vinicius.mascarenhas@outlook.com>
2024-02-13 22:02:42 -0300

    Correction element name

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ document.querySelector('#phone-checkbox')
 document.querySelector('button[type="submit"]')
   .addEventListener('click', function(event) {
     event.preventDefault()
-    const firstNameField = document.getElementById('firsme')
+    const firstNameField = document.getElementById('firstName')
     const lastNameField = document.getElementById('lastName')
     const emailField = document.getElementById('email')
     const textareaField = document.getElementById('open-text-area')
